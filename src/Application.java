76b8efb6e0d32249643d497311ad97751bf8e29a
Kirana <fransiskakirana@gmail.com>
2017-05-18 10:25:45 +0700

    Start

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Kirana on 18/05/2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hangman");
+    }
+}
---
+++ src/Hangman.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+/**
+ * Created by Kirana on 17/05/2017.
+ */
+public class Hangman extends JFrame {
+
+}
